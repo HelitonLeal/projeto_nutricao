29e4d11776986fd0b18aa5c47faa640da587f8b6
HelitonLeal <heliton.leal.84@gmail.com>
2022-04-19 20:59:45 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+export default function App(){
+  const [nutri, setNutri] = useState([]);
+
+  useEffect(() => {
+    function loadApi(){
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+      fetch(url)
+      .then((resultado) => resultado.json())
+      .then((json) => {
+        setNutri(json);
+      })
+    }
+
+    loadApi();
+  }, []);
+
+  return(
+    <div className="container">
+      <header>
+        <strong>PROJETO NUTRIÇÃO</strong>
       </header>
+      {nutri.map((item) => {
+        return(
+          <article key={item.id} className="post" >
+            <strong className="titulo">{item.titulo}</strong>
+            <img src={item.capa} alt={item.titulo} className="capa" />
+            <p className="subtitulo">
+              {item.subtitulo}
+            </p>
+            <a className="botao">Saiba Mais</a>
+          </article>
+        )
+      })}
     </div>
   );
 }
-
-export default App;
